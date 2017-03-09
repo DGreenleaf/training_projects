8990b2e6249d147fed604bc2ae0afbe319e10a38
Dmytro Plekhotkin <dplekhotkin@remit.se>
2017-03-09 15:32:03 +0200

    Proposals

--- src/start/practice/MyGame/Realization.java
+++ src/start/practice/MyGame/Realization.java
@@ -1,24 +1,40 @@
 package start.practice.MyGame;
 
+
 import java.util.Random;
+
 
 /**
  * Created by Sergiy on 3/5/17.
  */
 
+// DIMA: rename to more meaningful name.
+// DIMA: example: public class Game
+// DIMA: example2: public class TicTacToe
 public class Realization {
 
     //создадим двумерный массив поля ячеек размером 3х3 для нашей игры
 
+    // DIMA: make it private because it is used only in this class.
+
+    // DIMA: make name to be in UPPERCASE. It is common rule for constants. Read Java Specification: http://docs.oracle.com/javase/specs/jls/se8/html/jls-6.html#jls-6.1 (search for "Constant Names").
+    // DIMA: example: private static final char[][] GRID = new char[3][3];
     static final char[][] grid = new char[3][3];
 
 
     //заполним все элементы массива значениями пробелов (' ')
 
+    // DIMA: make the function private and invoke it in the constructor
+    // DIMA: example: public Realization() {
+    // 						initialization();
+    // 					}
+    // DIMA: for more information: https://en.wikipedia.org/wiki/Facade_pattern
     public void initialization() {
-        for (int i = 0; i < 3; i++)
-            for (int p = 0; p < 3; p++)
+        for (int i = 0; i < 3; i++) {
+            for (int p = 0; p < 3; p++) {
                 grid[i][p] = ' ';
+            }
+        }
     }
 
     //нарисуем игровое поле
@@ -37,6 +53,17 @@ public class Realization {
     //(если хотя бы одна из ячеек содержит значение ' ', то вернется false,
     // в другом случае все поле заполнено значениями)
 
+    // DIMA: methods that return boolean value should start from "is"
+    // example: public boolean isBoardFull() {...}
+
+    // DIMA: try not to use "magic numbers".
+    // 		From Wikipedia:
+    //			magic numbers - unique values with unexplained meaning
+    //								 or multiple occurrences
+    //								 which could (preferably) be replaced with named constants.
+    // In your case "3" is a magic number. You can use grid.length and grid[i].length instead
+
+    // DIMA: algorithm can be improved. For example, if the first cell is empty, algorithm will still continue to check other cells. It is redundant.
     public boolean boardFilling() {
         boolean isFull = true;
         for (int i = 0; i < 3; i++) {
@@ -51,12 +78,13 @@ public class Realization {
 
     //зададим условие, при котором назначается победа (все три значения в игровом поле равны и не пусты)
 
+    // DIMA: make the function private. It is used only internally
     public boolean checkValues(char v1, char v2, char v3) {
         return ((v1 != ' ') && (v1 == v2) && (v2 == v3));
     }
 
     //выполним проверку на победу при заполнении рядов (три значения в однмо ряду равны)
-
+    // DIMA: make the function private. It is used only internally
     public boolean checkRowForWin() {
         for (int i = 0; i < 3; i++)
 
@@ -69,7 +97,7 @@ public class Realization {
     }
 
     //выполним проверку на победу при заполнении колонок (три значения в одной колонке равны)
-
+    // DIMA: make the function private. It is used only internally
     public boolean checkColumnForWin() {
         for (int i = 0; i < 3; i++)
 
@@ -82,10 +110,9 @@ public class Realization {
     }
 
     //выполним проверку на победу при заполнении диагоналей (три значения по диагонали равны)
-
+    // DIMA: make the function private. It is used only internally
     public boolean checkDiagonalForWin() {
-        return ((checkValues(grid[0][0], grid[1][1], grid[2][2]))
-                || (checkValues(grid[0][2], grid[1][1], grid[2][0])));
+        return ((checkValues(grid[0][0], grid[1][1], grid[2][2])) || (checkValues(grid[0][2], grid[1][1], grid[2][0])));
     }
 
     // Проверка на общий результат игры. Возвращает true если победа, иначе возвращает false.
@@ -98,10 +125,11 @@ public class Realization {
     //Зададим порядок изменения игроков в игре.
 
     public char changePlayer(char player) {
-        if (player == 'x')
+        if (player == 'x') {
             player = 'o';
-        else
+        } else {
             player = 'x';
+        }
         return player;
     }
 
@@ -110,13 +138,11 @@ public class Realization {
     public boolean checkSymbol(int row, int column) {
 
         // проверяем, попадает ли координата положения значения в наше игровое поле
-        if ((row > 2 || column > 2) || (row < 0 || column < 0))
-
+        if ((row > 2 || column > 2) || (row < 0 || column < 0)) {
             return true;
-
-            //проверяем наличие в указанной точке символа 'x' или 'o'
-        else if (grid[row][column] == 'x' || grid[row][column] == 'o')
+        } else if (grid[row][column] == 'x' || grid[row][column] == 'o') {
             return true;
+        }
 
         return false;
     }
@@ -129,15 +155,6 @@ public class Realization {
 
     }
 
-    // ход допустим, если выполняется условие
-
-//    public boolean correctAction (int row, int column) {
-//        return row >= 0 && row < 3 && column >= 0 && column < 3
-//                &&
-//                grid[row][column] == 0;
-//    }
-
-
     //задаем правило, по которому ходит компьютер
 
     public int[] AI() { //начало метода AI
@@ -147,10 +164,20 @@ public class Realization {
         int randomValue[] = new int[2];
 
         do {
+            // DIMA: try not to use "magic numbers".
+            // 		From Wikipedia:
+            //			magic numbers - unique values with unexplained meaning
+            //								 or multiple occurrences
+            //								 which could (preferably) be replaced with named constants.
+            // In your case "3" is a magic number. You can use grid.length and grid[i].length instead
+
             randomValue[0] = random.nextInt(3);
             randomValue[1] = random.nextInt(3);
 
-        } while (!checkSymbol(randomValue[0], randomValue[1]));
+        }
+        // DIMA: I fixed condition here.
+        // was: while (!checkSymbol(randomValue[0], randomValue[1]));
+        while (checkSymbol(randomValue[0], randomValue[1]));
         return randomValue;
 
     } //окончание метода AI
